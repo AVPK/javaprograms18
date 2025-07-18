7cfa8fcc04af5cb9416ed3b6c016f2707805c266
AVPK <pranayabdas561@gmail.com>
2025-07-18 19:34:34 +0530

    Add files via upload

--- day1/Pi.java
+++ day1/Pi.java
@@ -6,7 +6,7 @@ public class Pi {
 	public static void main(String[] args) {
 		Pi P1 = new Pi();
 		System.out.println("PI:" + P1.Constant_Pi);
-		P1.Constant_Pi = 30;
+//		P1.Constant_Pi = 30;
 		System.out.println("PI:" + P1.Constant_Pi);
 	}
 }
